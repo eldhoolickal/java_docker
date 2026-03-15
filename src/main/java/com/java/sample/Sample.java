1af6f0508a07a2611ba5e9e01a1f45202fada378
eldhoolickal <eldhoolickal@gmail.com>
2026-03-15 13:34:29 +0530

    commit

--- src/main/java/com/java/sample/Sample.java
+++ src/main/java/com/java/sample/Sample.java
@@ -3,6 +3,8 @@ package com.java.sample;
 public class Sample {
 
     public static void main(String[] args) {
-        System.out.println("Hello World ");
+
+        System.out.println("Hello from Java running inside Docker");
+
     }
 }
